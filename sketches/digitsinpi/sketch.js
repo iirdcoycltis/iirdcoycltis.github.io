a6540da357ab9e8cae383c31d638114381db0e9f
Norman94 <jaxonpowe@gmail.com>
2022-02-14 20:44:59 -0500

    fixed

--- sketches/digitsinpi/sketch.js
+++ sketches/digitsinpi/sketch.js
@@ -1,14 +1,12 @@
 let pimr, digits, searchBox, indStat;
 let port = 3000;
 
-// function preload() {
-//   pimr = loadStrings("pi_million.txt");
-// }
+function preload() {
+  pimr = loadStrings("pi_million.txt");
+}
 
 function searchDigits() {
   let search = searchBox.value();
-  let url = `http://localhost:` + port + `/search/${search}`;
-  loadJSON(url, gotResult);
 }
 
 function gotResult(data) {
